182e8fe59d0ae3e19562c21df742cf3d8cd3346d
JamesPrenticez <Jamesprenticez@gmail.com>
2021-02-23 12:24:49 +1300

    connect-mongo V3.2.0

--- server.js
+++ server.js
@@ -5,19 +5,20 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-const routes = require('./routes');
-const auth = require('./auth.js');
+
+const routes = require('./routes')
+const auth = require('./auth')
 
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http)
 const passportSocketIo = require('passport.socketio');
 const cookieParser = require('cookie-parser');
 const MongoStore = require('connect-mongo')(session);
 const URI = process.env.MONGO_URI;
 const store = new MongoStore({ url: URI });
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -47,28 +48,40 @@ io.use(
   })
 );
 
+// Database connection
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
 
+  // Right after you establish a successful connection with the database, instantiate routes and auth like so:
   routes(app, myDataBase);
   auth(app, myDataBase);
 
-  let currentUsers = 0;
+  // Socket io listen with "on" inside our DB connection
+  
+  let currentUsers = 0
+  //Handel a user connecting
   io.on('connection', (socket) => {
-    ++currentUsers;
+    ++currentUsers
+    io.emit('user count', currentUsers)
+    console.log('A user has connected');
+  //Handel a user disconnecting
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected');
+    --currentUsers;
     io.emit('user count', currentUsers);
-    console.log('user ' + socket.request.user.username + ' connected');
-
-    socket.on('disconnect', () => {
-      console.log('A user has disconnected');
-      --currentUsers;
-      io.emit('user count', currentUsers);
-    });
   });
+});
+
+  // Catch Errors
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
   });
+});
+
+// App.listen -> HTTP.listen
+http.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port ' + process.env.PORT);
 });
 
 function onAuthorizeSuccess(data, accept) {
@@ -82,7 +95,3 @@ function onAuthorizeFail(data, message, error, accept) {
   console.log('failed connection to socket.io:', message);
   accept(null, false);
 }
-
-http.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port ' + process.env.PORT);
-});
